ac108d360f8155464f51ab6a0334daa4feff4f3f
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-12-18 23:56:37 +0800

    some few changes oki

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,24 @@
       clipped-left
     >
       <v-toolbar-title>Nightsong Armory</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>Built by <a href="https://carlomigueldy.com" target="_blank">carlomigueldy</a></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-content>
@@ -13,13 +31,13 @@
           <v-card-title>
             Guild Members
             <v-spacer></v-spacer>
-            <v-text-field
+            <!-- <v-text-field
               v-model="search"
               append-icon="mdi-magnify"
               label="Search"
               single-line
               hide-details
-            ></v-text-field>
+            ></v-text-field> -->
           </v-card-title>
           <v-data-table
             :headers="headers"
