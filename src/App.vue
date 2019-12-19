c7d7e97406dc17f58ced3285f676483ed7a7c579
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-12-19 09:46:53 +0800

    added Carousel

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
     <v-content>
       <v-container>
+        <app-carousel />
         <app-table />
       </v-container>
     </v-content>
@@ -13,12 +14,14 @@
 <script>
 import Navbar from './components/Navbar'
 import Table from './components/Table'
+import Carousel from './components/Carousel'
 
 export default {
   name: 'app',
   components: {
     'app-navbar': Navbar,
     'app-table': Table,
+    'app-carousel': Carousel,
   },
 }
 </script>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-carousel 
+        cycle 
+        interval="3000"
+        show-arrows-on-hover
+        hide-delimiters>
+        <v-carousel-item
+        v-for="(image, i) in images"
+        :key="i"
+        :src="image.link"
+        ></v-carousel-item>
+    </v-carousel>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        images: [
+            { link: '/img/carousel/carousel1.png' },
+            // { link: '/img/carousel/carousel2.jpg' },
+            { link: '/img/carousel/carousel3.png' },
+            // { link: '/img/carousel/carousel4.png' },
+            { link: '/img/carousel/carousel5.png' },
+            { link: '/img/carousel/carousel6.jpeg' },
+            { link: '/img/carousel/carousel7.jpeg' },
+            { link: '/img/carousel/carousel8.jpeg' },
+            { link: '/img/carousel/carousel9.png' },
+        ]
+    }),
+}
+</script>
